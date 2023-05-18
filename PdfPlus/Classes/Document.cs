fbeffe87c92c6584087a350d06ad8ff36fe74fe3
Alex Schiftner <alex@shapediver.com>
2023-05-18 21:50:15 +0200

    implement IGH_Goo for Document

--- PdfPlus/Classes/Document.cs
+++ PdfPlus/Classes/Document.cs
@@ -4,12 +4,14 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GH_IO.Serialization;
+using Grasshopper.Kernel.Types;
 
 using pdf = PdfSharp.Pdf;
 
 namespace PdfPlus
 {
-    public class Document
+    public class Document : IGH_Goo
     {
         #region members
 
@@ -110,6 +112,111 @@ namespace PdfPlus
         public override string ToString()
         {
             return "Document " + this.pages.Count + "pgs";
+        }
+
+        #endregion
+
+        #region IGH_Goo
+
+        public bool IsValid { get { return this.IsValidWhyNot == string.Empty; } }
+
+        public string IsValidWhyNot
+        {
+            get
+            {
+                if (this.pages.Count == 0)
+                {
+                    return "No pages";
+                }
+                else
+                {
+                    return string.Empty;
+                }
+            }
+        }
+
+        public string TypeName { get { return "PdfPlus"; } }
+
+        public string TypeDescription { get { return "Grasshopper type for PDF+ document"; } }
+
+        public IGH_Goo Duplicate()
+        {
+            return new Document(this);
+        }
+
+        public IGH_GooProxy EmitProxy()
+        {
+            return null;
+        }
+
+        public bool CastFrom(object source)
+        {
+            // Note: GH_Param<T>.Cast_Object(object data), which is used when adding data to a floating parameter, or
+            // when collecting data from sources, tries to use
+            //   target = InstantiateT();
+            //   target.CastFrom(data) ...
+            // before trying to use
+            //   data.CastTo<T>(out target)
+
+            if (source is Document)
+            {
+                CopyFrom((Document)source);
+                return true;
+            }
+            else if (source is IGH_Goo)
+            {
+                if (((IGH_Goo)source).CastTo(out Document document))
+                {
+                    CopyFrom((Document)source);
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public bool CastTo<T>(out T target)
+        {
+            target = default(T);
+      
+            if (typeof(T).IsAssignableFrom(typeof(pdf.PdfDocument)))
+            {
+                target = (T)((object)Bake());
+                return true;
+            }
+            else if (typeof(T).IsAssignableFrom(typeof(byte[])))
+            {
+                var obj = (object)GetByteArray();
+                if (obj == null)
+                    return false;
+                target = (T)obj;
+                return true;
+            }
+            else if (typeof(T).IsAssignableFrom(typeof(MemoryStream)))
+            {
+                var obj = (object)GetStream();
+                if (obj == null)
+                    return false;
+                target = (T)obj;
+                return true;
+            }
+
+            return false;
+        }
+
+        public object ScriptVariable()
+        {
+            return Bake();
+        }
+
+        public bool Write(GH_IWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Read(GH_IReader reader)
+        {
+            throw new NotImplementedException();
         }
 
         #endregion
